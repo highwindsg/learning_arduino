ac10e4ed5bd0557e0315f72e44858ce73275a857
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-12-31 19:45:29 +0800

    Update sketch_dec31c_ReadDatafromSerialPort.ino
    updates

--- ArduinoUno/sketch_dec31c_ReadDatafromSerialPort/sketch_dec31c_ReadDatafromSerialPort.ino
+++ ArduinoUno/sketch_dec31c_ReadDatafromSerialPort/sketch_dec31c_ReadDatafromSerialPort.ino
@@ -1,5 +1,6 @@
 int myNumber;
-
+String msg = "Please Enter Your Number: ";
+String msg2 = "Your Number is: ";
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +10,16 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial.println(msg);  // We ask.
+
+while (Serial.available() == 0){  // We wait.
+  
+}
+
+myNumber = Serial.parseInt(); // We read it.
+Serial.print(msg2); // Serial print out the first msg.
+Serial.println(myNumber); // Serial print out the second msg2.
+Serial.println("");
 
 }
 
